196495ad33bb138f8c759136c531e3f61c201bda
Logan D <logandufek@gmail.com>
2021-05-30 23:41:30 -0400

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt ("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
  
 var enemyName = "Roborto"
@@ -17,7 +18,15 @@ console.log(10 + 10);
 console.log("Our robot's name is " + playerName);
 
 var fight = function() {
-    window.alert("Welcome to Robot Gladiators!");
+ 
+ 
+window.alert("Welcome to Robot Gladiators!");
+    
+ 
+ var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
+ if (promptFight === "fight" || promptFight === "FIGHT") {
+   
 
   //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
   enemyHealth = enemyHealth - playerAttack;
@@ -49,8 +58,25 @@ if (playerHealth <= 0) {
   else {
     window.alert(playerName + " still has " + playerHealth + " health left.");
   }
-  
+ }
+ else if (promptFight === "skip" || promptFight === "SKIP") {
+    // confirm player wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+    // if yes (true), leave fight
+  if (confirmSkip) {
+    window.alert(playerName + " has decided to skip this fight. Goodbye!");
+    // subtract money from playerMoney for skipping
+    playerMoney = playerMoney - 2;
+  }
+  // if no (false), ask question again by running fight() again
+  else {
+    fight();
+  }
+    }
+ else {
+        window.alert("You need to choose a valid option. Try again!"); }
 };
 
+fight ()
+ 
 
-fight();
